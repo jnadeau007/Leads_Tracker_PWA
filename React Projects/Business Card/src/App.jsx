24813c362d0cea32c07c7ae2bc945207e6bc6cec
jnadeau007 <james4redsox@gmail.com>
2026-03-08 09:42:29 -0400

    Update business card app

---
+++ React Projects/Business Card/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Header from './assets/Components/Header.jsx'
+import MainContent from './assets/Components/MainContent.jsx'
+import Footer from './assets/Components/Footer.jsx'
+function App() {
+
+  return (
+    <div className= "container">
+      <Header />
+      <MainContent />
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
